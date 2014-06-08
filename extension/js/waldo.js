ac4f5727a97fb2da1818771226bd2e3d7a27efff
sasilukr <p.pandapower@gmail.com>
2014-06-07 19:34:17 -0700

    Convert HP Api calls to angular.js

--- extension/js/waldo.js
+++ extension/js/waldo.js
@@ -3,12 +3,37 @@ var app = angular.module('Waldo', []);
 app.controller('ChromeTab', ['$scope', 'IDOLService', function($scope, IDOLService) {
     $scope.test = "Hello World";
 
-    $scope.testButton = function() {
+    $scope.findSimilar = function() {
         var testURL = "http://automatic.com";
-        IDOLService.findSimilar(testURL).success(function(res) {
+        var indexName =  "news_eng";
+        IDOLService.findSimilar(testURL, indexName);
+
+        .success(function(res) {
+            console.log("Success: finding similar of URL " + testURL + " in index " + indexName + ". " );
             console.log(res);
         });
-    }
+    };
+
+    $scope.createIndex = function() {
+        var indexName =  "news_eng";
+        IDOLService.createIndex(indexName);
+
+        .success(function(res) {
+            console.log("Success: connecting to Create Text Index API : " + indexName + ". ");
+            console.log(res);
+        });
+    };
+
+    $scope.addIndex = function() {
+        var indexName =  "news_eng";
+        IDOLService.addIndex(url, indexName);
+
+        .success(function(res) {
+            console.log("Success: connecting to Add to Text Index API : " + indexName + " with URL : " + url);
+            console.log(res);
+        });
+    };
+
 }]);
 
 app.factory('IDOLService', function($http) {
@@ -21,15 +46,35 @@ app.factory('IDOLService', function($http) {
             addIndex: "https://api.idolondemand.com/1/api/sync/addtotextindex/v1?"
         };
 
-    service.findSimilar = function(url) {
+    service.findSimilar = function(url, indexName) {
 
         var apiRequest = api.findSimilar
             .concat("&apiKey=").concat(api.key)
             .concat("&url=").concat(url)
-            .concat("&indexes=").concat("news_eng");
+            .concat("&indexes=").concat(indexName);
 
-        console.log(apiRequest);
+        console.log("Find Similar Request: " + apiRequest);
+        return $http.get(apiRequest);
+    };
 
+    service.createIndex = function (indexName) {
+        var apiRequest = api.findSimilar
+            .concat("&apiKey=").concat(api.key)
+            .concat("&flavor=explorer")
+            .concat("&index=").concat(indexName);
+
+        console.log("Create Index Request: " + apiRequest);
+        return $http.get(apiRequest);
+
+    };
+
+    service.addIndex = function (url, indexName) {
+        var apiRequest = api.findSimilar
+            .concat("&apiKey=").concat(api.key)
+            .concat("&url=").concat(url)
+            .concat("&index=").concat(indexName);
+
+        console.log("Add Index Request: " + apiRequest);
         return $http.get(apiRequest);
     };
 
