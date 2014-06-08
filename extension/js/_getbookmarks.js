fb3848ac1a546be4662b177e017dac86932e15e3
Alexis Matherlee <alexismatherlee@gmail.com>
2014-06-07 18:42:42 -0700

    tabs

--- extension/js/_getbookmarks.js
+++ extension/js/_getbookmarks.js
@@ -1,12 +1,12 @@
 chrome.bookmarks.getTree(function(itemTree){
-itemTree.forEach(function(item){
-processNode(item);
-});
+	itemTree.forEach(function(item){
+		processNode(item);
+	});
 });
 
 function processNode(node) {
-if(node.children) {
-node.children.forEach(function(child) { processNode(child); });
+	if(node.children) {
+		node.children.forEach(function(child) { processNode(child); });
 }
 
 if(node.url) { console.log(node.url); }
